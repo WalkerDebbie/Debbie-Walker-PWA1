d5db80e692b0f596b592255a83d49d2f12daf82b
Walker_Debbie <dwalker1@fullsail.edu>
2015-01-20 13:47:24 -0500

    Updates

--- Apps/DuelApp-V0/js/main.js
+++ Apps/DuelApp-V0/js/main.js
@@ -12,11 +12,6 @@
      //old player name
     /*var playerOneName = "Spiderman"; //  declare a variable for player 1 to be the value of "Spiderman"
     var playerTwoName = "Batman";  // declare a variable for player 2 to be the value of "Batman"
-    */
-    // array
-    var fighter1=["Spiderman", "Batman"];
-
-
 
     //player damage
     var player1Damage = 20; //  declare a variable player 1 damage to be the value of 20
@@ -25,36 +20,41 @@
     //player health
     var playerOneHealth = 100; // declare  a variable player 1 health to be the value 100
     var playerTwoHealth = 100; // declare a variable player 2 health to be the value 100
+    */
 
-    var round=0; // declare the matching round to be a starting value of 0
+    // array
+    var fighter1=["Kabal", 20, 100]; //Define Array for Fighter 1
+    var fighter2=["Kratos", 20, 100];   //Define Array for Fighter 2
+
+    var round=1; // Define global variable for fighter rounds
 
     function fight(){ // this is a function called fight (it does not contain parameters)
-        console.log ('in the fight function'); // output/print "in the fight function"
+        console.log ("FIGHT!!!"); //Console log "FIGHT!!!
 
-        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth); // alert  user Player 1, Health START Player 2, Health
+        alert(fighter1[0]+":"+fighter1[2]+"  *START*  "+fighter2[0]+":"+fighter2[2]); // alert  user Player 1, Health START Player 2, Health
 
         for (var i=0; i < 10; i++){ // for loop used to determine how much damage for each round 
 
             //random formula is - Math.floor(Math.random() * (max - min) + min);
            
-           // nested "if/else" statments
+           // nested "if/else" statements
 
-            var minDamage1 = player1Damage * .5; // declare variable minDamage for player 1 to equal Player 1 damage * .5
-            var minDamage2 = player2Damage * .5;  // declare variable minDamage for player 2 to equal Player 1 damage * .5
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1); // variable f1 holds the value of random numbers from player 1 damage (max - min) + min
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2); // variable f2 holds the value of random numbers from player 2 damage (max - min) + min
+            var minDamage1 = fighter1[1] * .5; // declare variable minDamage for player 1 to equal Player 1 damage * .5
+            var minDamage2 = fighter2[1] * .5;  // declare variable minDamage for player 2 to equal Player 1 damage * .5
+            var f1 = Math.floor(Math.random()*(fighter1[1]-minDamage1)+minDamage1); // variable f1 holds the value of random numbers from player 1 damage (max - min) + min
+            var f2 = Math.floor(Math.random()*(fighter2[2]-minDamage2)+minDamage2); // variable f2 holds the value of random numbers from player 2 damage (max - min) + min
             //inflict damage
-            playerOneHealth-=f1; // variable Player One Health -= f1
-            playerTwoHealth-=f2; // varialbe Player Two Health -= f1
+            fighter1[2]-=f1; // variable Fighter One Health -= f1
+            fighter2[2]-=f2; // variable Fighter Two Health -= f1
 
-            console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth); // print Player 1's health and Player 2's health
+            console.log(fighter1[0]+":"+fighter1[2]+" "+fighter2[0]+":"+fighter2[2]); // print Fighter 1's name and health and Player 2's health
 
             var results = winnerCheck(); // variable results  = winnerCheck function            
             console.log(results); // print the results of the round
 
             if (results === "no winner"){ // if statement - if there is no winner...
                 round++; // round up 1
-                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER* "+playerTwoName+":"+playerTwoHealth); // alert the players that Player 1's health for previous rounds (#) is over and Player 2's health; players keep playing until round is false
+                alert(fighter1[0]+":"+fighter1[2]+"  *ROUND "+round+" OVER* "+fighter2[0]+":"+fighter2[2]); // alert the players that Player 1's health for previous rounds (#) is over and Player 2's health; players keep playing until round is false
             }else{ // else statement if the "if no winner" statement is false
                 alert(results); // print results
                 break; // break
@@ -64,17 +64,17 @@
 
     }; // closing curly braces for the function "fight"
 
-    function winnerCheck(){ // funciton winnerCheck
+    function winnerCheck(){ // function winnerCheck
         console.log("in winnerCheck FN"); // print to console "in winnerCheck FN"
 
         var result="no winner"; // variable result = to value "no winner"
 
-        if (playerOneHealth<1 && playerTwoHealth<1){ // if statement 
+        if (fighter1[2]<1 && fighter2[2]<1){ // if statement
             result = "You Both Die"; // if true; print "You Both Die"
-        } else if(playerOneHealth<1){ // or if Player 1's health is less than 1 (true)
-            result =playerTwoName+" WINS!!!" // print Player 2's (name) "WINS!!!"
-        } else if(playerTwoHealth<1){ // of if Player 2's health < 1 (true)
-            result =playerOneName+" WINS!!!" // print "WINS!!!"
+        } else if(fighter1[2]<1){ // or if Player 1's health is less than 1 (true)
+            result =fighter2[0]+" WINS!!!" // print Player 2's (name) "WINS!!!"
+        } else if(fighter2[2]<1){ // of if Player 2's health < 1 (true)
+            result =fighter1[0]+" WINS!!!" // print "WINS!!!"
         }; // closing curly braces for the if statement
 
         return result; // console results
@@ -82,7 +82,7 @@
     }; // closing curly braces for the winnerCheck function
 
     /*******  The program gets started below *******/
-    console.log('program starts') // console "program starts"
+    console.log('program starts'); // console "program starts"
     fight(); // call to function fight
 
-})(); // closing funciton
+})(); // closing function
