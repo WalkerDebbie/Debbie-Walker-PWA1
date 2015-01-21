1ded7b1038cf20a435e0e280e9fbc5172a59f220
Walker_Debbie <dwalker1@fullsail.edu>
2015-01-21 15:09:23 -0500

    Day 7 classwork complete

--- Apps/chapter6/codeDwalker.js
+++ Apps/chapter6/codeDwalker.js
@@ -1,4 +1,7 @@
-/*Debbie Walker*/
+console.log("Debbie Walker");
+document.write("Debbie Walker");
+
+/*
 
 var access = document.getElementById("code9");
 var code = access.innerHTML; 
@@ -6,14 +9,40 @@ code = code + " midnight";
 alert(code);
 
 
-/*var access =
+var access =
     document.getElementById("code9"); //gives you access to the object
 var code = access.innerHTML;//innerHTML property to gets access to the content to variable
 
 code = code + " midnight";
-alert(code); // code alerts with the passcode contained in the variable code
+alert(code); // code alerts with the pass code contained in the variable code
 
 */
 
-var myGreenText = document.getElementsByClassName("mygreentext");
-myGreenText.innerHTML = "Red Alert: hit by phaser fire!";
+function init() {
+    var myGreenText = document.getElementById("mygreentext");                  //pullsmygreentext varliable
+    myGreenText.innerHTML = "Green Alert: hit by phaser fire!";                //calls the html var
+    myGreenText.setAttribute("class", "greentext");
+
+    var myRedText = document.getElementById("myredtext");                  //pullsmygreentext varliable
+    myRedText.innerHTML = "Red Alert: hit by phaser fire!";                //calls the html var
+    myRedText.setAttribute("class", "redtext");
+
+    var myBlueText = document.getElementById("mybluetext");                  //pullsmygreentext varliable
+    myBlueText.innerHTML = "Blue Alert: hit by phaser fire!";                //calls the html var
+    myBlueText.setAttribute("class", "bluetext");
+
+    var Apples = document.getElementById("greenheader");                //changing the getElementId to greenheader overwrites the previous code in this sequential code function
+    Apples.innerHTML = "Green Planet";
+    Apples.setAttribute("class", "greentext");
+
+    var myRedHeader = document.getElementById("redheader");
+    myRedHeader.innerHTML = ("Red Planet");
+    myRedHeader.setAttribute("class", "redtext");
+
+    var myBlueHeader = document.getElementById("blueheader");
+    myBlueHeader.innerHTML = "Blue Planet";
+    myBlueHeader.setAttribute("class", "bluetext");
+}
+window.onload = init; // wrapping a variable into a method to call it will force the DOM to recognize this first
+
+
